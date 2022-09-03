bf1eb179b14c59363b26a3d751f355484ba6db5a
Francis <francisgomas721@gmail.com>
2022-09-03 20:59:57 +1200

    DBConnectionString

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,24 +10,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-string connectionString = null;
-//string envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
-string envVar = "postgres://noxxnfnoikpdbp:19335c476b9dd6aad340496df36d0ba7e6dcffb1b99991e60a5e007b96c17a71@ec2-35-170-21-76.compute-1.amazonaws.com:5432/d1ujltko3uuu3b";
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DataContext>(options =>
+{
+    /*var pgUserId = Environment.GetEnvironmentVariable("POSTGRES_USER_ID");
+    var pgPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+    var pgHost = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+    var pgPort = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+    var pgDatabase = Environment.GetEnvironmentVariable("POSTGRES_DB");*/
 
-var uri = new Uri(envVar);
-var username = uri.UserInfo.Split(':')[0];
-var password = uri.UserInfo.Split(':')[1];
-connectionString =
-"; Database=" + uri.AbsolutePath.Substring(1) +
-"; Username=" + username +
-"; Password=" + password +
-"; Port=" + uri.Port +
-";";
+    var pgUserId = "noxxnfnoikpdbp";
+    var pgPassword = "19335c476b9dd6aad340496df36d0ba7e6dcffb1b99991e60a5e007b96c17a71";
+    var pgHost = "ec2-35-170-21-76.compute-1.amazonaws.com";
+    var pgPort = "5432";
+    var pgDatabase = "d1ujltko3uuu3b";
 
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseNpgsql(connectionString)
-);
+    var connStr = $"Server={pgHost};Port={pgPort};User Id={pgUserId};Password={pgPassword};Database={pgDatabase}";
 
+    options.UseNpgsql(connStr);
+});
 
 var app = builder.Build();
 
