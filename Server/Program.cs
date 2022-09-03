b1ca3988de153dd273ba441fbb44fb5b75e9d274
Francis <francisgomas721@gmail.com>
2022-09-03 20:28:41 +1200

    DBConnectionString

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,31 +11,23 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 string connectionString = null;
-string envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
+//string envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
+string envVar = "postgres://noxxnfnoikpdbp:19335c476b9dd6aad340496df36d0ba7e6dcffb1b99991e60a5e007b96c17a71@ec2-35-170-21-76.compute-1.amazonaws.com:5432/d1ujltko3uuu3b";
 
-if (string.IsNullOrEmpty(envVar))
-{
-    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<DataContext>(options =>
-        options.UseSqlServer(connectionString)
-    );
-}
-else
-{
-    var uri = new Uri(envVar);
-    var username = uri.UserInfo.Split(':')[0];
-    var password = uri.UserInfo.Split(':')[1];
-    connectionString =
-    "; Database=" + uri.AbsolutePath.Substring(1) +
-    "; Username=" + username +
-    "; Password=" + password +
-    "; Port=" + uri.Port +
-    ";";
+var uri = new Uri(envVar);
+var username = uri.UserInfo.Split(':')[0];
+var password = uri.UserInfo.Split(':')[1];
+connectionString =
+"; Database=" + uri.AbsolutePath.Substring(1) +
+"; Username=" + username +
+"; Password=" + password +
+"; Port=" + uri.Port +
+";";
 
-    builder.Services.AddDbContext<DataContext>(options =>
-        options.UseNpgsql(connectionString)
-    );
-}
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(connectionString)
+);
+
 
 var app = builder.Build();
 
